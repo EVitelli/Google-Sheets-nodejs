d789122f6eb0a44aa62ffba4648374c4c23327ed
evitelli <erik.vitelli1@gmail.com>
2021-08-08 15:08:38 -0300

    initial settings

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Google-Sheets-nodejs
 Projeto que faz integração com o google sheets para editar planilhas
+
+Para fazer essa integração é necessário se ter um projeto no Google Cloud Platform (GCP)
+Link: https://developers.google.com/workspace/guides/create-project
+
+Com o projeto criado precimos criar as credenciais para acessar  a api
+Link: https://developers.google.com/workspace/guides/create-credentials
+
---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const { google } = require('googleapis');
+const auth = require('./auth');
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    auth.authorize(JSON.parse(content), listMajors);
+});
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+    const sheets = google.sheets({ version: 'v4', auth });
+    sheets.spreadsheets.values.get({
+        spreadsheetId: '185FdmijOJ18jYFBDoCw01awy0N5mgONCqkCrc9OXNtI',
+        range: 'Class Data!A2:E',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rows = res.data.values;
+        if (rows.length) {
+            console.log('Name, Major:');
+            // Print columns A and E, which correspond to indices 0 and 4.
+            rows.map((row) => {
+                console.log(`${row[0]}, ${row[4]}`);
+            });
+        } else {
+            console.log('No data found.');
+        }
+    });
+}
